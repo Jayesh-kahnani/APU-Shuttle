428fe2f1ed884c6a341ce9069d9f672f7d06cd50
Jayesh Kahnani <jayeshkahnani78@gmail.com>
2026-01-23 15:36:58 +0530

    Refactor Schedule component layout
    Updated the layout of the Schedule component to use a relative container for the iframe.

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const Schedule = () => {
   return (
-    <div className="w-full h-screen">
-      <iframe
-        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRPrHrPPDu07P_b9h13qQXAOu0Wc4TY1FUrkph4SY2ctTqRRlfOGhY-IsUF_s6S2TosKF8nhEcj26TF/pubhtml?gid=0&single=true"
-        className="w-full h-full border-0"
-        title="Schedule"
-      />
+    <div className="w-full h-[100dvh] overflow-hidden">
+      <div className="relative w-full h-full">
+        <iframe
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRPrHrPPDu07P_b9h13qQXAOu0Wc4TY1FUrkph4SY2ctTqRRlfOGhY-IsUF_s6S2TosKF8nhEcj26TF/pubhtml?gid=0&single=true"
+          className="absolute inset-0 w-full h-full border-0"
+          title="Schedule"
+        />
+      </div>
     </div>
   );
 };
