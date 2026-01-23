f7758840c7a2a98abfa77b2408d272ffbb44e618
Jayesh Kahnani <jayeshkahnani78@gmail.com>
2026-01-23 15:32:16 +0530

    Refactor Schedule component for React compatibility
    Updated class attributes to use className and added title to iframe.

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Schedule = () => {
-
   return (
-    <div class=" w-full h-screen">
+    <div className="w-full h-screen">
       <iframe
-        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRPrHrPPDu07P_b9h13qQXAOu0Wc4TY1FUrkph4SY2ctTqRRlfOGhY-IsUF_s6S2TosKF8nhEcj26TF/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
-        class="aspect-w-16 aspect-h-9 w-full h-full"
-      ></iframe>
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRPrHrPPDu07P_b9h13qQXAOu0Wc4TY1FUrkph4SY2ctTqRRlfOGhY-IsUF_s6S2TosKF8nhEcj26TF/pubhtml?gid=0&single=true"
+        className="w-full h-full border-0"
+        title="Schedule"
+      />
     </div>
   );
 };
